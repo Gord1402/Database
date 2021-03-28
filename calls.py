f68389401d38fca9524cf02fcb0a9aa1c22a72ae
Gord1402 <irinasergeenkova2@gmail.com>
2021-03-28 18:47:42 +0300

    initial commit

---
+++ calls.py
@@ -0,0 +1,10 @@
+class Calls(object):
+    
+    all = 0
+    author = "База заглушка\nСкачайте другие базы, чтобы бомбить"
+    
+    def __init__(self):
+        pass
+
+    def send(self, serv, phone):
+        raise     
